d3834acff469d20baed81db31810c6bd4cf12df7
Devin Henkel <devin@devinhenkel.com>
2016-04-14 12:25:31 -0500

    one last try...

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,116 @@
-// Ionic Starter App
+var app = angular.module('starter', ['ionic','ngCordova']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+app.controller('MotionController', function($scope, $ionicPlatform, $cordovaDeviceMotion) {
+
+    // watch Acceleration options
+    $scope.options = {
+        frequency: 100, // Measure every 100ms
+        deviation : 25  // We'll use deviation to determine the shake event, best values in the range between 25 and 30
+    };
+
+    // Current measurements
+    $scope.measurements = {
+        x : null,
+        y : null,
+        z : null,
+        timestamp : null
     }
-    if (window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
-      StatusBar.styleDefault();
+
+    // Previous measurements
+    $scope.previousMeasurements = {
+        x : null,
+        y : null,
+        z : null,
+        timestamp : null
     }
-  });
+
+    // Watcher object
+    $scope.watch = null;
+
+    // Start measurements when Cordova device is ready
+    $ionicPlatform.ready(function() {
+
+        //Start Watching method
+        $scope.startWatching = function() {
+
+            // Device motion configuration
+            $scope.watch = $cordovaDeviceMotion.watchAcceleration($scope.options);
+
+            // Device motion initilaization
+            $scope.watch.then(null, function(error) {
+                console.log(error);
+                $scope.errorMsg = error;
+            },function(result) {
+
+                // Set current data
+                $scope.measurements.x = result.x;
+                $scope.measurements.y = result.y;
+                $scope.measurements.z = result.z;
+                $scope.measurements.timestamp = result.timestamp;
+
+                // Detecta shake
+                $scope.detectShake(result);
+
+            });
+        };
+
+        // Stop watching method
+        $scope.stopWatching = function() {
+            $scope.watch.clearWatch();
+        }
+
+        // Detect shake method
+        $scope.detectShake = function(result) {
+
+            //Object to hold measurement difference between current and old data
+            var measurementsChange = {};
+
+            // Calculate measurement change only if we have two sets of data, current and old
+            if ($scope.previousMeasurements.x !== null) {
+                measurementsChange.x = Math.abs($scope.previousMeasurements.x, result.x);
+                measurementsChange.y = Math.abs($scope.previousMeasurements.y, result.y);
+                measurementsChange.z = Math.abs($scope.previousMeasurements.z, result.z);
+            }
+
+            // If measurement change is bigger then predefined deviation
+            if (measurementsChange.x + measurementsChange.y + measurementsChange.z > $scope.options.deviation) {
+                $scope.stopWatching();  // Stop watching because it will start triggering like hell
+                console.log('Shake detected'); // shake detected
+                setTimeout($scope.startWatching(), 1000);  // Again start watching after 1 sex
+
+                // Clean previous measurements after succesfull shake detection, so we can do it next time
+                $scope.previousMeasurements = {
+                    x: null,
+                    y: null,
+                    z: null
+                }
+
+            } else {
+                // On first measurements set it as the previous one
+                $scope.previousMeasurements = {
+                    x: result.x,
+                    y: result.y,
+                    z: result.z
+                }
+            }
+
+        }
+
+    });
+
+    $scope.$on('$ionicView.beforeLeave', function(){
+        $scope.watch.clearWatch(); // Turn off motion detection watcher
+    });
+
 });
