f0a4940ef5b994e63271f84d9a11411cb4c4590c
Devin Henkel <devin@devinhenkel.com>
2016-04-14 13:40:59 -0500

    added platforms

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/Acceleration.js",
+        "id": "cordova-plugin-device-motion.Acceleration",
+        "pluginId": "cordova-plugin-device-motion",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/accelerometer.js",
+        "id": "cordova-plugin-device-motion.accelerometer",
+        "pluginId": "cordova-plugin-device-motion",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device-motion": "1.2.0"
+}
+// BOTTOM OF METADATA
+});
